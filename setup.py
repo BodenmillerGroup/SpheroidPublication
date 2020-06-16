7c435c6250970426593a89d725ee635443a607bd
votti <mcvotti@gmail.com>
2020-06-16 14:53:43 +0200

    First commit of the spherpro analysis

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+        name='src',
+        packages=find_packages(),
+        version='0.1.0',
+        description='A spheroid dataset analysis',
+        author='Vito Zanotelli, vito.zanotelli (at) gmail.com',
+        license='MIT',
+)
